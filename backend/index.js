b0514219f6dc6ffcce9154de53ebf426ad3cdc00
a.krasnov <anton_krasnov_2001@mail.ru>
2022-03-08 20:36:15 +0300

    frontend + backend

---
+++ backend/index.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const PORT = 4000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`Server running on http://127.0.0.1:${PORT}/`))
+app.use(express.json())
+
+app.get('', ((req, res) => {
+    fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+        const response = files.map(file => {
+            return {
+                title: file
+            }
+        })
+        res.status(200).json(response)
+    })
+}))
+
+app.get('/:fileName', ((req, res) => {
+    const fileName = req.params.fileName
+    fs.readdir(path.resolve(__dirname, 'files'), (e, files) => {
+        if (files.some(file => file === fileName)) {
+            fs.readFile(path.resolve(__dirname, 'files', fileName), 'utf8', (er, data) => {
+                res.status(200).send(data)
+            })
+        } else res.status(200).json({message: 'Not found'})
+    })
+}))
+
+app.post('/', (req, res) => {
+    const content = JSON.stringify(req.body)
+    const fileName = String(Math.round(Math.random() * 1000)) + '.json'
+    fs.open(path.resolve(__dirname, 'files', fileName), 'w', (err) => {
+        if (err) throw err
+    })
+    fs.writeFile(path.resolve(__dirname, 'files', fileName), content, (err) => {
+        if (err) throw err
+    })
+    res.status(201).json({message: 'Success'})
+})
